72c7411d7befed7f7e3777af14498aac3162d19a
CHERUKURI VENKATESH <venkateshcherukuri91@gmail.com>
2026-08-14 14:35:04 +0530

    Time: 7 ms (54.7%), Space: 13.5 MB (13.55%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        self.prefix=[0]*len(nums)
+        self.prefix[0]=nums[0]
+        for i in range(1,len(nums)):
+            self.prefix[i]=self.prefix[i-1]+nums[i]
+        for j in range(0,len(nums)):
+            if j==0:
+                if self.prefix[len(nums)-1]-self.prefix[0]==0:
+                    return j
+                    break
+            elif self.prefix[j-1]==self.prefix[len(nums)-1]-self.prefix[j]:
+                return j
+                break
+        return -1
+            
+        
+        
