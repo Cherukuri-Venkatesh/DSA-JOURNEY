9a559593a1f1250065e67708b762776baa90cc0c
CHERUKURI VENKATESH <venkateshcherukuri91@gmail.com>
2026-08-14 15:11:57 +0530

    Time: 53 ms (96.65%), Space: 19.4 MB (55.58%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        arr=[0]*(2*n)
+        for i in range(2*n):
+            if i%2==0:
+                arr[i]=nums[i//2]
+            else :
+                arr[i]=nums[n+(i//2)]
+        return arr
+        
