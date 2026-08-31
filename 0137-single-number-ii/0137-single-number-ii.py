ccc8d1ff690301f27dff08d614a9c959468a4f88
CHERUKURI VENKATESH <venkateshcherukuri91@gmail.com>
2026-08-31 19:12:15 +0530

    Time: 27 ms (28.88%), Space: 20.7 MB (36.54%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans=0
+        for i in range(32):
+            c=0
+            for num in nums:
+                if (num>>i)&1:
+                    c+=1
+                
+            if c%3!=0:
+                ans=ans|(1<<i)
+            if ans>=(1<<31):
+                ans-=(1<<32)
+        return ans
+
