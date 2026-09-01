ac94fe9f55370ba4fe941ad9c07699af6753b126
CHERUKURI VENKATESH <venkateshcherukuri91@gmail.com>
2026-09-01 08:31:20 +0530

    Time: 63 ms (12.57%), Space: 20.3 MB (17.63%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans=[]
+        for i in range(n+1):
+            c=0
+            while i>0:
+                if i&1:
+                    c+=1
+                i=i>>1
+            ans.append(c)
+        return ans
+
+        
