badeb130e3ced97107cd80547a59bd131d421b9e
CHERUKURI VENKATESH <venkateshcherukuri91@gmail.com>
2026-09-04 20:24:45 +0530

    Time: 0 ms (100%), Space: 19.3 MB (56.96%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,15 +1,11 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        a=""
-        for i in range(len(digits)):
-            a+=str(digits[i])
-        a=int(a)
-        a=a+1
-        a=str(a)
-        ans=[]
-        for ch in a:
-            ans.append(int(ch))
-        return ans
+        for i in range(len(digits)-1,-1,-1):
+            if digits[i]<9:
+                digits[i]+=1
+                return digits
+            digits[i]=0
+        return [1]+digits
 
 
 
