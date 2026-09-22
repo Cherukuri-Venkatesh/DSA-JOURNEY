613e87b28b51b5c3090a0e96223ccccc2d3eaaea
CHERUKURI VENKATESH <venkateshcherukuri91@gmail.com>
2026-09-22 07:29:45 +0530

    Time: 0 ms (100%), Space: 19.7 MB (82.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,22 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        seen={}
-        for i in range(len(nums)):
-            complement=target-nums[i]
-            if complement in seen:
-                return [seen[complement],i]
-                break
-            seen[nums[i]]=i
+        original=nums
+        nums1=sorted(nums)
+        p1=0
+        p2=len(nums1)-1
+        while p1<p2:
+            sum1=nums1[p1]+nums1[p2]
+            if sum1==target:
+                ind=original.index(nums1[p1])
+                if nums1[p1] == nums1[p2]:
+                    ind2=original.index(nums1[p2], ind + 1)
+                else:
+                    ind2=original.index(nums1[p2])
+                return [ind,ind2]
+            elif sum1<target:
+                p1+=1
+            else:
+                p2-=1
+        return []
+        
         
