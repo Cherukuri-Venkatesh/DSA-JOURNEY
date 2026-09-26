1f5958d5f8d188d0ce0e1e4c9ac1eab366380361
CHERUKURI VENKATESH <venkateshcherukuri91@gmail.com>
2026-09-26 18:38:35 +0530

    Time: 0 ms (100%), Space: 31.3 MB (75.07%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: list[int]) -> int:
+        l=0
+        h=len(arr)-1
+        while l<h:
+            m=(l+h)//2
+            if arr[m]>=arr[m+1]:
+                h=m
+            else:
+                l=m+1
+        return l
+
+        
